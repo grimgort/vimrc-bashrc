1103890889f2183a834b5869ceaec0206f079b9f
Tarroux <frederic.tarroux@onera.fr>
2020-07-16 17:49:56 +0200

    update for dta616

--- forWindows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ forWindows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -17,3 +17,34 @@ Get-Content $tempFile | Foreach-Object {
 Remove-Item $tempFile
 
 . (Join-Path -Path (Split-Path -Parent -Path $PROFILE) -ChildPath $(switch($HOST.UI.RawUI.BackgroundColor.ToString()){'White'{'Set-SolarizedLightColorDefaults.ps1'}'Black'{'Set-SolarizedDarkColorDefaults.ps1'}default{return}}))
+
+
+
+
+Import-Module posh-git
+Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
+#function Start-gvimdiff{
+#Start-Process "D:\ftarroux\Logiciel\Vim64\vim81\gvim.exe" -d
+#}
+#function vimrcmod{
+#Start-Process "D:\ftarroux\Logiciel\Vim64\vim81\gvim.exe" ~/.vimrc 
+#}
+
+
+#New-Alias gvim D:\ftarroux\Logiciel\Vim64\vim81\gvim.exe
+#New-Alias frvimrc vimrcmod
+#New-Alias gvimdiff Start-gvimdiff
+New-Alias frproxy ~/proxyPowerShell.ps1
+
+function frperso { set-location "D:\ftarroux\Documents\FRED"}
+
+New-Alias frbash D:\ftarroux\Documents\WindowsPowerShell\Script\frbash.ps1
+New-Alias archive_function D:\ftarroux\Documents\WindowsPowerShell\Script\archiveGit.ps1
+
+# Chocolatey profile
+#$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+#if (Test-Path($ChocolateyProfile)) {
+#  Import-Module "$ChocolateyProfile"
+#}
+
+
