079231a5c85e6e9bb0efa27a9dc29b6ac8f59c09
tarroux <frederic.tarroux@onera.fr>
2020-07-16 10:59:07 +0200

    adding scoop backupe

---
+++ forWindows/backup-scoop.ps1
@@ -0,0 +1,43 @@
+try{if(Get-Command scoop){}} catch {iex (new-object net.webclient).downloadstring('https://get.scoop.sh')}
+scoop install git
+scoop bucket add extras
+scoop bucket add knox-scoop https://git.irs.sh/KNOXDEV/knox-scoop
+scoop bucket add main
+scoop bucket add minorgod https://github.com/minorgod/scoop-minorgod.git
+scoop install 7zip
+scoop install autohotkey
+scoop install cmake
+scoop install Console2
+scoop install copyQ
+scoop install ctags
+scoop install dark
+scoop install deepl
+scoop install discord
+scoop install doxygen
+scoop install gcc
+scoop install git
+scoop install googlechrome
+scoop install D:\ftarroux\scoop\workspace\gradle.json
+scoop install languagetool
+scoop install lessmsi
+scoop install libreoffice-stable
+scoop install Link-Shell-Extension
+scoop install llvm
+scoop install mupdf
+scoop install neovim
+scoop install ninja
+scoop install nodejs
+scoop install pandoc
+scoop install perl
+scoop install posh-git
+scoop install pwsh
+scoop install qt-creator
+scoop install rambox
+scoop install scoop-backup
+scoop install teamviewer
+scoop install vim
+scoop install vscode
+scoop install watchman-nightly
+scoop install wsltty
+scoop install yarn
+
