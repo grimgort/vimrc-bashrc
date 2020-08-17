9e78dcf4ee369266ef44311e0ae7123613160bdc
tarroux <frederic.tarroux@onera.fr>
2020-08-17 10:38:03 +0200

    add gitconfiginstall

--- install.py
+++ install.py
@@ -1,7 +1,9 @@
 import os
 from os.path import expanduser
+import shutil
+
+local_path = os.path.abspath(os.path.dirname(__file__))
 def install_vim():
-    local_path = os.path.abspath(os.path.dirname(__file__))
     print("install  .vimrc")
     f = open(os.path.join(expanduser("~") , ".vimrc"),"w")
     f.write("source " + os.path.join(local_path , "forWindows/vimrcWindows \n"))
@@ -10,7 +12,17 @@ def install_vim():
     f.close()
     print("process finish")
 
+def install_ginit():
+    print("install ginit")
+    shutil.copy2(os.path.join(local_path , "forWindows/ginit.vim"),os.path.join(expanduser("~") , "AppData/Local/nvim") )
+    
+def install_gitconfig():
+    print("install gitconfig")
+    shutil.copy2(os.path.join(local_path , "forWindows/.gitconfig"),expanduser("~") )
+
 def main():
     install_vim()
+    install_ginit()
+    install_gitconfig()
 
 main()
