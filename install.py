be3dda9cc052a193ed8cd7ee7a13cf17f1893f9b
tarroux <frederic.tarroux@onera.fr>
2020-08-17 10:26:23 +0200

    add install explanduser

--- install.py
+++ install.py
@@ -1,7 +1,9 @@
 import os
+from os.path import expanduser
 def install_vim():
     local_path = os.path.abspath(os.path.dirname(__file__))
-    f = open(".vimrc2","w")
+    print("install  .vimrc")
+    f = open(os.path.join(expanduser("~") , ".vimrc"),"w")
     f.write("source " + os.path.join(local_path , "forWindows/vimrcWindows \n"))
     f.write("nnoremap <leader>v :e " + os.path.join(local_path , "forWindows/vimrcWindows") +"<cr> \n")
     f.write("let g:coc_config_home= \"" + os.path.join(local_path , "forWindows")+"\" \n")
