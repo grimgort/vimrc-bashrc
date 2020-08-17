f1f018890adfe165fe92b9d4d8dbc3df81c01ecc
tarroux <frederic.tarroux@onera.fr>
2020-08-17 11:04:55 +0200

    frmscv86 added

--- forWindows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ forWindows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,20 @@
-$tempFile = [IO.Path]::GetTempFileName()  
+#$tempFile = [IO.Path]::GetTempFileName()  
 
-## Store the output of cmd.exe.  We also ask cmd.exe to output   
-## the environment table after the batch file completes  
+### Store the output of cmd.exe.  We also ask cmd.exe to output   
+### the environment table after the batch file completes  
 
-cmd /c " `"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat`" && set > `"$tempFile`" " 
+#cmd /c " `"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat`" && set > `"$tempFile`" " 
 
-## Go through the environment variables in the temp file.  
-## For each of them, set the variable in our local environment.  
-Get-Content $tempFile | Foreach-Object {   
-    if($_ -match "^(.*?)=(.*)$")  
-    { 
-        Set-Content "env:\$($matches[1])" $matches[2]  
-    } 
-}  
+### Go through the environment variables in the temp file.  
+### For each of them, set the variable in our local environment.  
+#Get-Content $tempFile | Foreach-Object {   
+    #if($_ -match "^(.*?)=(.*)$")  
+    #{ 
+        #Set-Content "env:\$($matches[1])" $matches[2]  
+    #} 
+#}  
 
-Remove-Item $tempFile
+#Remove-Item $tempFile
 
 . (Join-Path -Path (Split-Path -Parent -Path $PROFILE) -ChildPath $(switch($HOST.UI.RawUI.BackgroundColor.ToString()){'White'{'Set-SolarizedLightColorDefaults.ps1'}'Black'{'Set-SolarizedDarkColorDefaults.ps1'}default{return}}))
 
@@ -40,6 +40,8 @@ function frperso { set-location "D:\ftarroux\Documents\FRED"}
 
 New-Alias frbash D:\ftarroux\Documents\WindowsPowerShell\Script\frbash.ps1
 New-Alias archive_function D:\ftarroux\Documents\WindowsPowerShell\Script\archiveGit.ps1
+
+New-Alias frmscv86 D:\ftarroux\Documents\WindowsPowerShell\Script\frmscv86.ps1
 
 # Chocolatey profile
 #$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
