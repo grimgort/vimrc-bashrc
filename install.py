a3136cb0f815b780afdb7384e5f5e6ed9f146658
tarroux <frederic.tarroux@onera.fr>
2020-08-17 10:21:43 +0200

    fix install

--- install.py
+++ install.py
@@ -1,8 +1,14 @@
 import os
 def install_vim():
-    local_path = os.path.dirname(__file__)
+    local_path = os.path.abspath(os.path.dirname(__file__))
     f = open(".vimrc2","w")
-    f.write("source " + os.path.join(local_path , "forWindows/vimrcWindows"))
-    f.write("nnoremap <leader>v :e " + os.path.join(local_path , "forWindows/vimrcWindows") +"<cr>")
-    f.write("let g:coc_config_home= \"" + os.path.join(local_path , "forWindows")+"\"")
+    f.write("source " + os.path.join(local_path , "forWindows/vimrcWindows \n"))
+    f.write("nnoremap <leader>v :e " + os.path.join(local_path , "forWindows/vimrcWindows") +"<cr> \n")
+    f.write("let g:coc_config_home= \"" + os.path.join(local_path , "forWindows")+"\" \n")
     f.close()
+    print("process finish")
+
+def main():
+    install_vim()
+
+main()
