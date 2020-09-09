0b567244713facf1e07014d8a6bc079ccb2d6541
tarroux <frederic.tarroux@onera.fr>
2020-09-09 13:22:56 +0200

    scoop backup

---
+++ scoop/backup-200909.ps1
@@ -0,0 +1,46 @@
+try{if(Get-Command scoop){}} catch {iex (new-object net.webclient).downloadstring('https://get.scoop.sh')}
+scoop install git
+scoop bucket add extras
+scoop bucket add knox-scoop https://git.irs.sh/KNOXDEV/knox-scoop
+scoop bucket add main
+scoop bucket add minorgod https://github.com/minorgod/scoop-minorgod.git
+scoop install 7zip
+scoop install autohotkey
+scoop install cmake
+scoop install Console2
+scoop install copyQ
+scoop install ctags
+scoop install dark
+scoop install deepl
+scoop install discord
+scoop install doxygen
+scoop install eclipse-cpp
+scoop install fd
+scoop install gcc
+scoop install git
+scoop install googlechrome
+scoop install languagetool
+scoop install lessmsi
+scoop install libreoffice-stable
+scoop install Link-Shell-Extension
+scoop install llvm
+scoop install mupdf
+scoop install neovim
+scoop install ninja
+scoop install nodejs
+scoop install nssm
+scoop install pandoc
+scoop install perl
+scoop install posh-git
+scoop install pwsh
+scoop install python
+scoop install qt-creator
+scoop install rambox
+scoop install scoop-backup
+scoop install teamviewer
+scoop install vim
+scoop install vscode
+scoop install windows-terminal
+scoop install wsltty
+scoop install yarn
+
